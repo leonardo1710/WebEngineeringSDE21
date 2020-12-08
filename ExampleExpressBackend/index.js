5a57f6f5312c3d3c30ed359440eec0bc73477bf5
Leon Freudenthaler <leon.freudenthaler@stud.fh-campuswien.ac.at>
2020-12-08 12:46:24 +0100

    Example Express Backend

--- ExampleExpressBackend/index.js
+++ ExampleExpressBackend/index.js
@@ -1,8 +1,5 @@
-
-const routes = require('./routes');
-
-const bodyParser = require('body-parser');  // enables accessing the payload of http post requests
 const express = require('express');
+const bodyParser = require('body-parser');  // enables accessing the payload of http post requests
 const cors = require('cors');
 
 const app = express();
@@ -12,15 +9,13 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-require('./models/user');
-
-app.use(require('./routes'));
+app.use(require('./routes')); // routes module
 
 /*
 app.get('/', (req, res) => {
   res.send('Hello World from express');
 });
 */
+
 app.listen(port, () => console.log(`Hello world on port ${port}`));
 
--- ExampleExpressBackend/routes/api/user.js
+++ ExampleExpressBackend/routes/api/user.js
@@ -1,44 +1,48 @@
+const auth = require('../../common/auth');
 var users = require('../../models/user');
 const express = require('express');
 
 const router = express.Router();
 
+const accessTokenSecret = 'youraccesstokensecret'; // private key - should never be shared with anyone
+const jwt = require('jsonwebtoken');
+
 router.post('/login', (req, res) => {
   const user = req.body;
 
-  if(!req.body.email && 
-      !req.body.password) {
-    return res.status(400).send({
-      success: 'false',
-      message: 'email and password required.'
-    });
+  if(!user.email && !user.password) {
+    return sendResponse(res, 400, 'email and password required.', false);
   } else {
 
     let userExists = users.filter(user => user.email === req.body.email);
 
     if(userExists.length > 0){
-      if(userExists[0].password === req.body.password){
-        return res.status(200).send({
-          success: 'true',
-          message: userExists[0]
-        });
+      if(userExists[0].password === user.password){
+
+        const accessToken = jwt.sign({ username: userExists.username,  role: userExists.role }, accessTokenSecret);
+        const message = {
+          user: userExists[0],
+          token: accessToken
+        }
+        return sendResponse(res, 200, message, true);
       } else {
-        return res.status(400).send({
-          success: 'false',
-          message: 'password is wrong'
-        });
+        return sendResponse(res, 400, 'password is wrong', false);
       }
     } else {
-      return res.status(400).send({
-        success: 'false',
-        message: 'user does not exist'
-      });
+      return sendResponse(res, 400, 'user does not exist', false);
     }
   }
 });
 
-router.get('/getAll', (req, res) => {
+router.get('/getAll', auth, (req, res) => {
   res.json(users);
 })
+
+function sendResponse(response, statusCode, message, success){
+  return response.status(statusCode).send({
+    success: success,
+    message: message
+  });
+}
 
 module.exports = router;
